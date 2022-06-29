e1b10484110eecacfe03611636c6624970820a6d
Nicolas Cristian <n3kk.br0s@gmail.com>
2022-06-29 15:44:48 -0300

    Finished

--- 01/a_03.py
+++ 01/a_03.py
@@ -17,6 +17,22 @@ class Match:
   def get_goals_home(self):
     return(self.home_goals)
 
+  def get_winner(self):
+    # return self.home if self.home_goals > self.away_goals else self.away
+    if self.home_goals == self.away_goals:
+      return 'Draw'
+    elif self.home_goals > self.away_goals:
+      return self.home
+    else:
+      return self.away
+
+  def get_loser(self):
+    if self.home_goals == self.away_goals:
+      return 'Draw'
+    elif self.home_goals > self.away_goals:
+      return self.away
+    else:
+      return self.home
 
 class Standings:
   matches = {}
@@ -31,11 +47,50 @@ class Standings:
     for i, m in enumerate(self.matches):
       self.matches[i].details()
 
-  def most_home_losses():
-    print('todo')
+  def home_defeats(self):
+    defeats = {}
+    for i, match in self.matches.items():
+      loser = match.get_loser()
+      if loser != 'Draw':
+        if loser not in defeats:
+          defeats[loser] = 1
+        else:
+          defeats[loser] = defeats[loser] + 1
+    return defeats
 
-  def most_wins():
-    print('todo')
+  def most_home_defeats(self):
+    defeats = self.home_defeats()
+
+    team = max(defeats, key=defeats.get)
+    defeats = max(defeats.values())
+    victories = self.match_wins()[team]
+
+    # Note, the exercise asked to return the VICTORY count for the team with lose the most as home.
+    # Thats what I'm doing with 'victories' var.
+    # I'm not sure if it was a mistype... But, if you want to see how many defeats it had
+    # Just comment the following line and uncomment the line below.
+    print(f'\nThe team with the most defeats at home was {team}, with {victories} match(es) won.')
+    # print(f'\nThe team with the most defeats at home was {team}, with {defeats} matches lost.')
+
+  def match_wins(self):
+    wins = {}
+    for i, match in self.matches.items():
+      winner = match.get_winner()
+      # print(f'Match nº {i}, winner: {winner}')
+      if winner != 'Draw':
+        if ( winner not in wins):
+          wins[winner] = 1
+        else:
+          wins[winner] = wins[winner] + 1
+    return wins
+
+  def most_wins(self):
+    wins = self.match_wins();
+
+    team = max(wins, key=wins.get)    
+    wons = max(wins.values())
+
+    print(f'\nThe team with the most victories was {team}, with {wons} matches won.')
 
   def home_matches(self):
     hms = {}
@@ -59,7 +114,7 @@ class Standings:
     for i, ma in enumerate(self.matches):
       goals_total += self.matches[i].home_goals + self.matches[i].away_goals
     goals_average = goals_total / self.matches_total
-    print(f'On the {self.matches_total} matches, {goals_total} were scored. The average goals count per match was: {goals_average:.1f}.')
+    print(f'\nOn the {self.matches_total} matches, {goals_total} were scored. The average goals count per match was: {goals_average:.1f}.')
 
   def most_goals_match(self):
     match_num, goal_record = 0, 0
@@ -84,7 +139,7 @@ matches = [
   [7, "Flamengo", "Corinthians", 2, 8],
   [8, "Flamengo", "Gremio", 0, 0],
   [9, "Flamengo", "Bahia", 1, 0],
-  [10, "Bahia", "Corinthians", 0, 0],
+  [10, "Bahia", "Corinthians", 20, 0],
   [11, "Bahia", "Gremio", 1, 0],
   [12, "Bahia", "Flamengo", 1, 0]
 ]
@@ -92,13 +147,18 @@ matches = [
 standings = Standings(matches)
 
 # To list every match and it's details
-# standings.all_matches()
+standings.all_matches()
 
+# Team with most defeats in home
+standings.most_home_defeats()
 
-print('\nAverage goals by home team:')
+# Team with most wins
+standings.most_wins()
+
+# print('\nAverage goals by home team:')
+print('\nAverage goal count by home team:')
 print(standings.average_home_goals())
 
-print('\nAverage goals by match:')
 standings.average_matches_goals()
 
 standings.most_goals_match()
